ecfbfb0db5a602a8732facb6288f1ac991621aca
rtmrslnv <artemaraslanov4@gmail.com>
2024-12-20 19:02:43 +0500

    Экспорт отчета в excel(раздел "Численность обучающихся" и "Расписание занятий")

--- ReStart/administration/utils.py
+++ ReStart/administration/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 from reports.models import Organization, Event, convert_to_dict
 from user.models import User
 
+
 def export_to_excel_common(session, writer):
     result = []
     users = session.query(User).filter(User.is_admin==False).all()
@@ -12,8 +13,6 @@ def export_to_excel_common(session, writer):
                                                 .order_by(Organization.creation_time.desc()).first()
         events =  session.query(Event).filter(Event.organization_id==report_changed.id)
         for event in events:
-            user = session.query(User).filter(User.organization_id==report_changed.organization_id).first()
-
             name = f'{user.second_name} {user.first_name} {user.last_name}' if user is not None else 'Не определено'
             hours_total = sum([report_changed.hours_mon, report_changed.hours_tue, 
                             report_changed.hours_wed, report_changed.hours_thu, 
@@ -49,4 +48,45 @@ def export_to_excel_common(session, writer):
         'Кол-во участников', 'Дата проведения мероприятия', 'Уровень проводимого мероприятия', 'Место проведения',
         'Организатор', 'Положение', 'Достижения ШСК'
     ])
-    df.to_excel(writer, sheet_name='Общий')
+    df.to_excel(writer, sheet_name='Общий', index=False)
+
+def export_to_excel_schedule(session, writer):
+    result = []
+    users = session.query(User).filter(User.is_admin==False).all()
+    for user in users:
+        report_changed = session.query(Organization).filter(Organization.organization_id==user.organization_id)\
+                                                .order_by(Organization.creation_time.desc()).first()
+        result.append([
+            user.municipality_name, report_changed.name, 
+            report_changed.hours_mon, report_changed.hours_tue, 
+            report_changed.hours_wed, report_changed.hours_thu, 
+            report_changed.hours_fri, report_changed.hours_sat, 
+            report_changed.hours_sun
+        ])
+
+    df = pd.DataFrame(result, columns=[
+        'Муниципальное образование', 'Школьный спортивный клуб', 'Понедельник',
+        'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'
+    ])
+    df.to_excel(writer, sheet_name='Расписание занятий', index=False)
+
+def export_to_excel_student_count(session, writer):
+    result = []
+    users = session.query(User).filter(User.is_admin==False).all()
+    for user in users:
+        report_changed = session.query(Organization).filter(Organization.organization_id==user.organization_id)\
+                                                .order_by(Organization.creation_time.desc()).first()
+        result.append([
+            user.municipality_name, report_changed.name, 
+            report_changed.students_total, report_changed.students_grade_1, report_changed.students_grade_2, 
+            report_changed.students_grade_3, report_changed.students_grade_4, report_changed.students_grade_5, 
+            report_changed.students_grade_6, report_changed.students_grade_7, report_changed.students_grade_8, 
+            report_changed.students_grade_9, report_changed.students_grade_10, report_changed.students_grade_11
+        ])
+
+    df = pd.DataFrame(result, columns=[
+        'Муниципальное образование', 'Школьный спортивный клуб', 'Всего', 
+        '1 класс', '2 класс', '3 класс', '4 класс', '5 класс', '6 класс', '7 класс',
+        '8 класс', '9 класс', '10 класс', '11 класс'
+    ])
+    df.to_excel(writer, sheet_name='Численность обучающихся', index=False)
