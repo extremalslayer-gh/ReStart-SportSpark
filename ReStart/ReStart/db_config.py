b07644414ab3b6b221a20981d58049b442f2e57f
rtmrslnv <artemaraslanov4@gmail.com>
2025-01-04 18:47:26 +0500

    Временные изменения для упрощения деплоя

--- ReStart/ReStart/db_config.py
+++ ReStart/ReStart/db_config.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-USER = ''
-PASSWORD = ''
-SCHEMA = ''
-HOST = ''
-PORT = 5432
-DATABASE_URL = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{SCHEMA}'
+#USER = 'postgres'
+# Также является паролем админа
+PASSWORD = 'ReStart_temp_pass'
+SCHEMA = 'postgres'
+#HOST = ''
+#PORT = 5432
+#DATABASE_URL = f'postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{SCHEMA}'
+
+# pythonanywhere запрещает подключения к сторонним сервисам, временное решение - использовать sqlite
+DATABASE_URL = 'sqlite:///restart.db'
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
